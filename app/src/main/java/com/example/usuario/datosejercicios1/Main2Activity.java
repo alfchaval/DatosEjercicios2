cad926086092ef8443c4201b36a81a66fa4660be
alfchaval <alfchaval@hotmail.com>
2017-10-03 12:47:38 +0200

    2

--- app/src/main/java/com/example/usuario/datosejercicios1/Main2Activity.java
+++ app/src/main/java/com/example/usuario/datosejercicios1/Main2Activity.java
@@ -4,8 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
